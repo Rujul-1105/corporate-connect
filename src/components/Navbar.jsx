96a0728077f7763d51a7b5472c8e1221a996ec42
Harsh <harsh2005dalal@gmail.com>
2025-03-22 00:57:44 +0530

    Home page Complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,13 +7,15 @@ const Navbar = () => {
     <div>
       <div className="main">
         <a href="#"><div><img className="logoccimg" src={logocc} alt="logocc" /></div></a>
-        <div className="links">
-          <div><a href="#">Visiting companies</a></div>
-          <div><a href="#">Gallery</a></div>
-          <div><a href="#">Contact Us</a></div>
+        <div className='flex flex-row gap-10'>
+          <div className="links flex flex-row gap-4">
+            <div><a href="#">Visiting companies</a></div>
+            <div><a href="#">Gallery</a></div>
+            <div><a href="#">Contact Us</a></div>
+          </div>
+          <a href="#"><img className='logoiitimg' src={logoiit} alt="logoiit" /></a>
+          {/* <div className="logoiit"></div> */}
         </div>
-        <a href="#"><img className='logoiitimg' src={logoiit} alt="logoiit" /></a>
-        {/* <div className="logoiit"></div> */}
       </div>
     </div>
   )
