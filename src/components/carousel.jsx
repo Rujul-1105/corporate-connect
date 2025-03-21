5baaa6f5938b54dce038f9f91fade6e0d2e85456
Rujul Matta <rujulmatta36@gmail.com>
2025-03-21 17:45:43 +0000

    minor-updates

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -7,109 +7,111 @@ import 'swiper/css/pagination';
 const Carousel = () => {
   return (
     <div
-      className="min-h-screen flex flex-col items-center justify-center bg-cover bg-no-repeat bg-center relative px-6 py-10 border-b-2 border-white"
+      className="min-h-screen flex flex-col items-center justify-center bg-cover  bg-no-repeat bg-center relative px-6 py-10 border-b-2 border-white"
       style={{ backgroundImage: 'url("/images/backdrop_c1.png")' }}
     >
       {/* Text Section */}
-      <div className="w-80% text-white bg-black/25 p-6 shadow-lg order-1 my-5">
-        <p className="text-l leading-relaxed">
-          Corporate Connect is an industry-academia interaction event
-          hosted by IIT Ropar that provides a platform to industry
-          leaders, renowned academics, and ambitious students to
-          foster meaningful connections, facilitate knowledge-sharing,
-          and inspire innovative approaches to tackle the challenges of
-          today’s dynamic industrial landscape.
-          Today’s corporations are global, complex, and interconnected,
-          crossing domains, cultures, and technologies. In this era,
-          success isn’t about being a 'jack of all trades' or a 'master of
-          one'—it’s about becoming a master of many. The modern
-          engineer or innovator must blend expertise across disciplines,
-          from mechanics to AI, to truly thrive. Corporate Connect at IIT
-          Ropar aims to be the bridge linking industries, academia, and
-          aspiring leaders, creating a dynamic space where knowledge
-          meets innovation. This ambitious initiative is our commitment
-          to shaping tomorrow’s multidisciplinary trailblazers, ready for
-          a rapidly evolving world
-          Corporate Connect 3.0 – our continued effort to bridge the
-          gap between classroom knowledge and the real-world
-          demands of tomorrow. This year, we’re taking future leaders
-          straight to the source, offering firsthand insights from top
-          industry experts on breakthrough technologies and the
-          evolving fusion of engineering fields. Join us and step into the
-          future, where knowledge meets innovation and leadership is
-          redefined.
-        </p>
-      </div>
-      {/* Glassmorphic Carousel Container */}
-      <div className="max-w-8xl w-7xl bg-white/20 backdrop-blur-md p-6 rounded-xl shadow-lg order-2">
+      <div className="bg-white/20 backdrop-blur-md ">
+        <div className="w-80% text-white p-6  order-1 my-5">
+          <p className="text-l leading-relaxed">
+            Corporate Connect is an industry-academia interaction event
+            hosted by IIT Ropar that provides a platform to industry
+            leaders, renowned academics, and ambitious students to
+            foster meaningful connections, facilitate knowledge-sharing,
+            and inspire innovative approaches to tackle the challenges of
+            today’s dynamic industrial landscape.
+            Today’s corporations are global, complex, and interconnected,
+            crossing domains, cultures, and technologies. In this era,
+            success isn’t about being a 'jack of all trades' or a 'master of
+            one'—it’s about becoming a master of many. The modern
+            engineer or innovator must blend expertise across disciplines,
+            from mechanics to AI, to truly thrive. Corporate Connect at IIT
+            Ropar aims to be the bridge linking industries, academia, and
+            aspiring leaders, creating a dynamic space where knowledge
+            meets innovation. This ambitious initiative is our commitment
+            to shaping tomorrow’s multidisciplinary trailblazers, ready for
+            a rapidly evolving world
+            Corporate Connect 3.0 – our continued effort to bridge the
+            gap between classroom knowledge and the real-world
+            demands of tomorrow. This year, we’re taking future leaders
+            straight to the source, offering firsthand insights from top
+            industry experts on breakthrough technologies and the
+            evolving fusion of engineering fields. Join us and step into the
+            future, where knowledge meets innovation and leadership is
+            redefined.
+          </p>
+        </div>
+        {/* Glassmorphic Carousel Container */}
+        <div className="max-w-8xl w-7xl p-6 rounded-xl shadow-lg order-2 m-auto">
 
-        <Swiper
-          modules={[Navigation, Pagination, Autoplay]}
-          spaceBetween={10}
-          slidesPerView={1}
-          navigation
-          pagination={{ clickable: true }}
-          autoplay={{ delay: 3000, disableOnInteraction: false }}
-          loop={true}
-          className="rounded-xl"
-        >
-          <SwiperSlide>
-            <img
-              src="/images/5.png"
-              alt="Slide 1"
-              className="w-7xl h-[500px] object-cover rounded-lg"
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img
-              src="/images/2.png"
-              alt="Slide 2"
-              className="w-7xl h-[500px] object-cover rounded-lg"
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img
-              src="/images/3.png"
-              alt="Slide 3"
-              className="w-7xl h-[500px] object-cover rounded-lg"
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img
-              src="/images/4.png"
-              alt="Slide 3"
-              className="w-7xl h-[500px] object-cover rounded-lg"
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img
-              src="/images/5.png"
-              alt="Slide 3"
-              className="w-7xl h-[500px] object-cover rounded-lg"
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img
-              src="/images/6.png"
-              alt="Slide 3"
-              className="w-7xl h-[500px] object-cover rounded-lg"
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img
-              src="/images/7.png"
-              alt="Slide 3"
-              className="w-7xl h-[500px] object-cover rounded-lg"
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img
-              src="/images/8.png"
-              alt="Slide 3"
-              className="w-7xl h-[500px] object-cover rounded-lg"
-            />
-          </SwiperSlide>
-        </Swiper>
+          <Swiper
+            modules={[Navigation, Pagination, Autoplay]}
+            spaceBetween={10}
+            slidesPerView={1}
+            navigation
+            pagination={{ clickable: true }}
+            autoplay={{ delay: 1000, disableOnInteraction: false }}
+            loop={true}
+            className="rounded-xl"
+          >
+            <SwiperSlide>
+              <img
+                src="/images/5.png"
+                alt="Slide 1"
+                className="w-7xl h-[500px] object-cover rounded-lg"
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img
+                src="/images/2.png"
+                alt="Slide 2"
+                className="w-7xl h-[500px] object-cover rounded-lg"
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img
+                src="/images/3.png"
+                alt="Slide 3"
+                className="w-7xl h-[500px] object-cover rounded-lg"
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img
+                src="/images/4.png"
+                alt="Slide 3"
+                className="w-7xl h-[500px] object-cover rounded-lg"
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img
+                src="/images/5.png"
+                alt="Slide 3"
+                className="w-7xl h-[500px] object-cover rounded-lg"
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img
+                src="/images/6.png"
+                alt="Slide 3"
+                className="w-7xl h-[500px] object-cover rounded-lg"
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img
+                src="/images/7.png"
+                alt="Slide 3"
+                className="w-7xl h-[500px] object-cover rounded-lg"
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img
+                src="/images/8.png"
+                alt="Slide 3"
+                className="w-7xl h-[500px] object-cover rounded-lg"
+              />
+            </SwiperSlide>
+          </Swiper>
+        </div>
       </div>
     </div>
   );
